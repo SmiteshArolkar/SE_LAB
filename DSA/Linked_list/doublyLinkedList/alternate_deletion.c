caf5a616349d20950a412be4a5fb268ce17187ad
ProgramingHub(dont know what i m dong :D) <111310544+Spooderman177@users.noreply.github.com>
2022-10-25 22:54:55 +0530

    Exp-6-2,3

---
+++ DSA/Linked_list/doublyLinkedList/alternate_deletion.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <malloc.h>
+struct NODE
+{
+    int info;
+    struct NODE *next;
+};
+typedef struct NODE node;
+node *altdel(node *, int);
+node *createlist(node *, int);
+void display(node *);
+node *createlist(node *last, int size)
+{
+    if (size > 0)
+    {
+        int data;
+        node *tmp;
+        printf("enter data :");
+        scanf("%d", &data);
+        tmp = (node *)malloc(sizeof(node));
+        tmp->info = data;
+        tmp->next = tmp;
+        last = tmp;
+        for (int i = 1; i < size; i++)
+        {
+            printf("enter data :");
+            scanf("%d", &data);
+            tmp = (node *)malloc(sizeof(node));
+            tmp->info = data;
+            tmp->next = last->next;
+            last->next = tmp;
+            last = tmp;
+        }
+        return last;
+    }
+}
+void display(node *last)
+{
+    if (last == NULL)
+    {
+        printf("empty list ....\n");
+        return;
+    }
+    node *tmp = last->next;
+    do
+    {
+        printf("%d ", tmp->info);
+        tmp = tmp->next;
+    } while (tmp != last->next);
+}
+node *altdel(node *last, int n)
+{
+    if (last == NULL)
+        printf("empty list :");
+    if (last->next == last)
+        return last;
+    else
+    {
+        node *tmp = last->next;
+        do
+        {
+            node *q = tmp->next;
+            tmp->next = q->next;
+            free(q);
+            tmp = tmp->next;
+        } while (tmp->next != last);
+        if (n & 1)
+            return last;
+        else
+        {
+            tmp->next = last->next;
+            free(last);
+            return tmp;
+        }
+    }
+}
+int main()
+{
+    int n;
+    node *last = NULL;
+    printf("enter the number of nodes required : ");
+    scanf("%d", &n);
+    last = createlist(last, n);
+    printf("the original list is : ");
+    display(last);
+    last = altdel(last, n);
+    printf("\nthe list after deletion is : ");
+    display(last);
+    return 0;
+}
+
+/*
+OP : /tmp/X3O3lvQdUP.o
+enter the number of nodes required : 4
+enter data :1
+enter data :2
+enter data :3
+enter data :4
+the original list is : 1 2 3 4 
+the list after deletion is : 1 3 
+*/
---
+++ DSA/Linked_list/doublyLinkedList/doubly_2.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+struct NODE
+{
+    int info;
+    struct NODE *next;
+    struct NODE *prev;
+};
+typedef struct NODE node;
+node *createlist(node *, int);
+node *addbegin(node *, int);
+node *add_end(node *, int);
+node *sum(node *, node *, node *);
+void display(node *);
+node *createlist(node *start, int n)
+{
+    int data;
+    data = n % 10;
+    start = addbegin(start, data);
+    n = n / 10;
+    while (n > 0)
+    {
+        data = n % 10;
+        start = add_end(start, data);
+        n = n / 10;
+    }
+    return start;
+}
+node *addbegin(node *start, int data)
+{
+    node *temp;
+    temp = (node *)malloc(sizeof(node));
+    temp->info = data;
+    if (start == NULL)
+    {
+        temp->next = NULL;
+        temp->prev = NULL;
+        start = temp;
+        return start;
+    }
+    else
+    {
+        temp->next = start;
+        temp->prev = NULL;
+        start->prev = temp;
+        start = temp;
+        return start;
+    }
+}
+node *add_end(node *start, int data)
+{
+    node *temp = start, *p;
+    p = (node *)malloc(sizeof(node));
+    p->info = data;
+    if (start == NULL)
+    {
+        start = p;
+        p->prev = NULL;
+        p->next = NULL;
+    }
+    while (temp->next != NULL)
+        temp = temp->next;
+    temp->next = p;
+    p->next = NULL;
+    p->prev = temp;
+    return start;
+}
+void display(node *start)
+{
+    node *temp = start;
+    while (temp->next != NULL)
+        temp = temp->next;
+    do
+    {
+        printf("%d", temp->info);
+        temp = temp->prev;
+    } while (temp != NULL);
+}
+node *sum(node *temp1, node *temp2, node *startadd)
+{
+    int carry = 0;
+    startadd = addbegin(startadd, ((temp1->info + temp2->info) % 10) + carry);
+    carry = (temp1->info + temp2->info) / 10;
+    temp1 = temp1->next;
+    temp2 = temp2->next;
+    while (temp1 != NULL || temp2 != NULL)
+    {
+        if (temp1 != NULL && temp2 != NULL)
+        {
+            startadd = add_end(startadd, (((temp1->info + temp2->info) % 10) + carry));
+            carry = (temp1->info + temp2->info) / 10;
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+        else if (temp1 == NULL)
+        {
+            while (temp2 != NULL)
+            {
+                startadd = add_end(startadd, temp2->info + carry);
+                carry = 0;
+                temp2 = temp2->next;
+            }
+        }
+        else
+        {
+            while (temp1 != NULL)
+            {
+                startadd = add_end(startadd, temp1->info + carry);
+                carry = 0;
+                temp1 = temp1->next;
+            }
+        }
+    }
+    if (carry)
+        startadd = add_end(startadd, carry);
+    return startadd;
+}
+int main()
+{
+    node *start1, *start2, *startadd;
+    start1 = start2 = startadd = NULL;
+    int n1, n2;
+    printf("enter the first number : ");
+    scanf("%d", &n1);
+    printf("enter the second number : ");
+    scanf("%d", &n2);
+    start1 = createlist(start1, n1);
+    start2 = createlist(start2, n2);
+    printf("the first list is : ");
+    display(start1);
+    printf("\nthe second list is : ");
+    display(start2);
+    startadd = sum(start1, start2, startadd);
+    printf("\nthe sum is : ");
+    display(startadd);
+    return 0;
+}
+/*
+OP : 
+/tmp/X3O3lvQdUP.o
+enter the first number : 4
+enter the second number : 5
+the first list is : 4
+the second list is : 5
+the sum is : 9
+*/
